c50a7b2895e149f879522d3ffc7194f749cf1fa8
Andrea Garcia <andreagarcia749@gmail.com>
2024-07-02 17:11:51 -0400

    clean up imports and alphabetize some libs. keep testing lib just in case

--- app/src/main/java/com/andrea/pianocompanionroom/view/UploadMidiScreen.kt
+++ app/src/main/java/com/andrea/pianocompanionroom/view/UploadMidiScreen.kt
@@ -7,8 +7,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.andrea.pianocompanionroom.R
--- app/src/main/java/com/andrea/pianocompanionroom/viewmodel/UploadMidiViewModel.kt
+++ app/src/main/java/com/andrea/pianocompanionroom/viewmodel/UploadMidiViewModel.kt
@@ -1,12 +1,9 @@
 package com.andrea.pianocompanionroom.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.andrea.pianocompanionroom.data.SongsRepository
 import com.andrea.pianocompanionroom.data.convertMidiToSong
-import com.andrea.pianocompanionroom.data.parseMidiFile
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import java.io.InputStream
 import javax.inject.Inject
 
